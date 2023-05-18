ff23462981f25eec048a0cc12048ecf246d24469
VaNomad <sjur.hassel@gmail.com>
2023-05-18 11:32:25 +0100

    Adds and starts build branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-      <div>js-ca</div>
-    </>
-  )
+  return <div>js-ca</div>;
 }
 
-export default App
+export default App;
