32d746b964846fd8c6abb7dec6791472a46232e1
VaNomad <sjur.hassel@gmail.com>
2023-05-20 15:49:48 +0100

    Adds testfilter to Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,8 @@ export default function Home() {
     {title: 'Headphones', body: 'lorem ipsum...', author: 'Anders', id: 1 },
     {title: 'Guitar', body: 'lorem ipsum...', author: 'Betty', id: 2 },
     {title: 'Backpack', body: 'lorem ipsum...', author: 'Cicci', id: 3 },
+    {title: 'Binoculars', body: 'lorem ipsum...', author: 'Cicci', id: 4 },
+    {title: 'Skis', body: 'lorem ipsum...', author: 'Cicci', id: 5 },
   ]);
 
 
@@ -15,8 +17,8 @@ export default function Home() {
     <div className="h-screen flex flex-col items-center text-black">
       <h1 className=" text-4xl font-bold">Home Page</h1>
       <div className="text-xl">{ '<' } searchbar placeholder { '>' }</div>
+      <ProductList products={products.filter((product) => product.author === 'Cicci')} title="Cicci's Products" />
       <ProductList products={products} title="All Products" />
-      <ProductList products={products} title="Cicci's Products" />
       {/* <Card /> */}
     </div>
   );
