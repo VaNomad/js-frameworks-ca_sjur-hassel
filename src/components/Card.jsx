9342f526e14142d8c34f757cb0f000951e4c7582
VaNomad <sjur.hassel@gmail.com>
2023-05-23 15:03:50 +0100

    Destructures product in cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,28 @@
-export default function Card({products, title}) {
+import { Link } from "react-router-dom";
+
+export default function Card({ products, title, id, }) {
   return (
     <>
-      <h2 className="mt-8 mb-2 font-semibold uppercase bg-black text-white p-3">
-        {title}{" "}
-      </h2>
-      <div className="flex flex-wrap justify-center">
-        <div className="grid grid-cols-3 gap-3 justify-center items-center text-black mb-8">
-          {products.map((product) => (
-            <div
-              className="card-zoom flex flex-col justify-center items-center text-center p-4 border border-gray-400 w-full h-full m-3"
-              key={product.id}
-            >
-              <div className="card-zoom-image">
-                <img src={ product.imageUrl } alt={ product.title } /></div>
-              <h2 className="card-zoom-text font-bold">{product.title}</h2>
-            </div>
-          ))}
+      <Link to={`/product/${id}`}>
+        <h2 className="mt-8 mb-2 font-semibold uppercase bg-black text-white p-3">
+          {title}
+        </h2>
+        <div className="flex flex-wrap justify-center">
+          <div className="grid grid-cols-3 gap-3 justify-center items-center text-black mb-8">
+            {products.map(({ id, title, imageUrl }) => (
+              <div
+                className="card-zoom flex flex-col justify-center items-center text-center p-4 border border-gray-400 w-full h-full m-3"
+                key={id}
+              >
+                <div className="card-zoom-image">
+                  <img src={imageUrl} alt={title} />
+                </div>
+                <h2 className="card-zoom-text font-bold">{title}</h2>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
