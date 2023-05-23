538c74e8f145b52c88c2003a9c4d2d5e6cd89ab1
VaNomad <sjur.hassel@gmail.com>
2023-05-23 15:02:54 +0100

    formats footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,7 @@
 export default function Footer() {
   return (
     <div className="h-screen flex justify-center items-center text-4xl text-black font-bold">
-      <h1></h1>Footer</div>
-  )
+      <h1>Footer</h1>
+    </div>
+  );
 }
