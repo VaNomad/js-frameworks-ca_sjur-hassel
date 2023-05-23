a7013dd6ed24ce5526e269511c8e3f75fdb377ce
VaNomad <sjur.hassel@gmail.com>
2023-05-23 11:32:31 +0100

    renders imageUrl correctly as src

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,24 @@
-export default function Card() {
+export default function Card({products, title}) {
   return (
-    <div className="flex flex-wrap justify-center">
-      <div className="card-zoom">
-        <div className="card-zoom-image"></div>
-        <h1 className="card-zoom-text">placeholder</h1>
+    <>
+      <h2 className="mt-8 mb-2 font-semibold uppercase bg-black text-white p-3">
+        {title}{" "}
+      </h2>
+      <div className="flex flex-wrap justify-center">
+        <div className="grid grid-cols-3 gap-3 justify-center items-center text-black mb-8">
+          {products.map((product) => (
+            <div
+              className="card-zoom flex flex-col justify-center items-center text-center p-4 border border-gray-400 w-full h-full m-3"
+              key={product.id}
+            >
+              <div className="card-zoom-image">
+                <img src={ product.imageUrl } alt={ product.title } /></div>
+              <h2 className="card-zoom-text font-bold">{product.title}</h2>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 // import Card from "../components/Card";
 
 import { useState, useEffect } from "react";
-import ProductList from "../components/ProductList";
+// import ProductList from "../components/ProductList";
 import Card from "../components/Card";
 
 const url = "https://api.noroff.dev/api/v1/online-shop";
@@ -17,17 +17,27 @@ export default function Home() {
       setProducts(json);
     }
     getData();
+    
   }, []);
+
+  console.log(products);
 
 
   return (
     <div className="h-screen flex flex-col items-center text-black">
       <h1 className=" text-4xl font-bold">Home Page</h1>
-      <div className="text-xl">{ '<' } searchbar placeholder { '>' }</div>
-      {/* <ProductList products={products.filter((product) => product.author === 'Cicci')} title="Cicci's Products" /> */}
-      <ProductList products={ products } title="All Products" />
-      <Card />
-      {/* <Card /> */}
+      <div className="text-xl">
+        {"<"} searchbar placeholder {">"}
+      </div>
+      {products.length > 0 && <Card products={products} title="All Products" />}
     </div>
   );
+}
+
+
+{
+  /* <ProductList products={products.filter((product) => product.author === 'Cicci')} title="Cicci's Products" /> */
+}
+{
+  /* <ProductList products={products} title="All Products" /> */
 }
