137118cf46dc1721a1cafaed3e3564ce53073fe8
VaNomad <sjur.hassel@gmail.com>
2023-05-23 10:11:37 +0100

    Adds API call for products on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,32 @@
 // import Card from "../components/Card";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProductList from "../components/ProductList";
+import Card from "../components/Card";
+
+const url = "https://api.noroff.dev/api/v1/online-shop";
 
 export default function Home() {
-  const [products, setProducts] = useState([
-    {title: 'Headphones', body: 'lorem ipsum...', author: 'Anders', id: 1 },
-    {title: 'Guitar', body: 'lorem ipsum...', author: 'Betty', id: 2 },
-    {title: 'Backpack', body: 'lorem ipsum...', author: 'Cicci', id: 3 },
-    {title: 'Binoculars', body: 'lorem ipsum...', author: 'Cicci', id: 4 },
-    {title: 'Skis', body: 'lorem ipsum...', author: 'Cicci', id: 5 },
-  ]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(url);
+      const json = await response.json();
+
+      setProducts(json);
+    }
+    getData();
+  }, []);
 
 
   return (
     <div className="h-screen flex flex-col items-center text-black">
       <h1 className=" text-4xl font-bold">Home Page</h1>
       <div className="text-xl">{ '<' } searchbar placeholder { '>' }</div>
-      <ProductList products={products.filter((product) => product.author === 'Cicci')} title="Cicci's Products" />
-      <ProductList products={products} title="All Products" />
+      {/* <ProductList products={products.filter((product) => product.author === 'Cicci')} title="Cicci's Products" /> */}
+      <ProductList products={ products } title="All Products" />
+      <Card />
       {/* <Card /> */}
     </div>
   );
