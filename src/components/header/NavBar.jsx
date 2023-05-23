9cb8019e3f05a975f2d0ebd436415a8a77cf6522
VaNomad <sjur.hassel@gmail.com>
2023-05-23 10:11:18 +0100

    Style update to NavBar

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
       <nav className="flex justify-between p-3 h-[130px]">
         {/* Left Box */}
         <div className="h-full">
-          <ul className="flex flex-col h-full justify-between">
+          <ul className="flex flex-col xs:flex-row xs:gap-8 h-full justify-between">
             <li>
               <NavLink
                 to="/"
@@ -35,7 +35,7 @@ export default function NavBar() {
           </ul>
         </div>
         {/* Right Box */}
-        <div className="flex flex-col h-full justify-between">
+        <div className="flex flex-col xs:flex-row-reverse xs:gap-4 h-full justify-between">
           <div className="flex justify-end">
             <Link to="/checkout">
               <Cart />
