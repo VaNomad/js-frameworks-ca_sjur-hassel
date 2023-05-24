c4e73df8ed4b76092d40da2095a69bf00fc746fe
VaNomad <sjur.hassel@gmail.com>
2023-05-24 15:47:05 +0100

    spinner size update

--- src/utils/Spinner.jsx
+++ src/utils/Spinner.jsx
@@ -2,8 +2,8 @@ import { Triangle } from "react-loader-spinner";
 
 export default function Spinner() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Triangle height={100} width={100} color="green" />
+    <div className="h-screen flex justify-center items-center text-green-600">
+      <Triangle height={200} width={200} />
     </div>
   );
 }
