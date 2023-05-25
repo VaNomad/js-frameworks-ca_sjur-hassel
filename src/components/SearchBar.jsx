49aa6e4343d72021389d24cc1e9b203374c524b7
VaNomad <sjur.hassel@gmail.com>
2023-05-25 10:24:12 +0100

    Adds comments + moves onclick + del. sep. tab open

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,17 @@
+// Imports —————————————————————————————————————————————————————
 import { useState, useEffect } from "react";
 import { BsSearch } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
+// Component —————————————————————————————————————————————————————
 export default function SearchBar() {
   const [search, setSearch] = useState("");
   const [searchData, setSearchData] = useState([]);
   const [selectedItem, setSelectedItem] = useState(-1);
   const navigate = useNavigate();
 
+  // Handlers
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
@@ -35,6 +38,7 @@ export default function SearchBar() {
     }
   };
 
+  // Call
   useEffect(() => {
     if (search !== "") {
       const url = `https://api.noroff.dev/api/v1/online-shop?q=${search}`;
@@ -46,23 +50,22 @@ export default function SearchBar() {
 
   return (
     <>
+      {/* Search Bar */}
       <div className="relative w-full border border-y-black border-y-2">
         <form action="">
           <div className="flex justify-between overflow-hidden rounded bg-white shadow-sm shadow-gray-400">
             <input
               type="text"
               placeholder="Search for Items . . ."
-              autoComplete="off"
+              autoComplete="on"
               onChange={handleChange}
               value={search}
               onKeyDown={handleKeyDown}
-              onClick={() =>
-                navigate(`/details/${searchData[selectedItem].id}`)
-              }
               className="block w-full flex-1 py-2 px-3 focus:outline-none"
             />
+            {/* Search & Close Buttons */}
             <div className="flex">
-              {search === "" ? (
+              { search === "" ? (
                 <div
                   className="m-1 p-2 cursor-pointer items-center rounded bg-green-600 hover:bg-green-500
                hover:scale-105 transition-all duration-200"
@@ -80,19 +83,19 @@ export default function SearchBar() {
             </div>
           </div>
         </form>
+        {/* Search Suggestions Drop-Down */}
         <div className="absolute z-10 flex flex-col mt-2 p-2 overflow-hidden rounded bg-white">
           {searchData.map((data, index) => {
             return (
               <a
                 href={data}
                 key={index}
-                target="_blank"
-                rel="noreferrer"
                 className={
                   selectedItem === index
                     ? "search-line my-1 active"
                     : "searchline"
                 }
+                onClick={() => navigate(`/details/${data.id}`)}
               >
                 {data.title}
               </a>
