195179c1c136df55b512b0420d08258a6711e4e7
VaNomad <sjur.hassel@gmail.com>
2023-05-25 12:35:17 +0100

    removes form div

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -40,7 +40,7 @@ export default function SearchBar() {
 
   const handleOnClick = () => {
     navigate(`/details/${searchData[selectedItem].id}`);
-  }
+  };
 
   // Call
   useEffect(() => {
@@ -56,37 +56,36 @@ export default function SearchBar() {
     <>
       {/* Search Bar */}
       <div className="relative w-full border border-y-black border-y-2">
-        <form action="">
-          <div className="flex justify-between overflow-hidden rounded bg-white shadow-sm shadow-gray-400">
-            <input
-              type="text"
-              placeholder="Search for Items . . ."
-              autoComplete="on"
-              onChange={handleChange}
-              value={search}
-              onKeyDown={handleKeyDown}
-              className="block w-full flex-1 py-2 px-3 focus:outline-none"
-            />
-            {/* Search & Close Buttons */}
-            <div className="flex">
-              { search === "" ? (
-                <div
-                  className="m-1 p-2 cursor-pointer items-center rounded bg-green-600 hover:bg-green-500
+        <div className="flex justify-between overflow-hidden rounded bg-white shadow-sm shadow-gray-400">
+          <input
+            type="text"
+            placeholder="Search for Items . . ."
+            autoComplete="on"
+            onChange={handleChange}
+            value={search}
+            onKeyDown={handleKeyDown}
+            className="block w-full flex-1 py-2 px-3 focus:outline-none"
+          />
+          {/* Search & Close Buttons */}
+          <div className="flex">
+            {search === "" ? (
+              <div
+                className="m-1 p-2 cursor-pointer items-center rounded bg-green-600 hover:bg-green-500
                hover:scale-105 transition-all duration-200"
-                >
-                  <BsSearch size={25} color="white" />
-                </div>
-              ) : (
-                <div
-                  className="m-1 p-2 inline-flex cursor-pointer items-center rounded bg-fuchsia-600 hover:bg-fuchsia-400
+              >
+                <BsSearch size={25} color="white" />
+              </div>
+            ) : (
+              <div
+                className="m-1 p-2 inline-flex cursor-pointer items-center rounded bg-fuchsia-600 hover:bg-fuchsia-400
                hover:scale-105 transition-all duration-200"
-                >
-                  <IoMdClose size={25} color="white" onClick={handleClose} />
-                </div>
-              )}
-            </div>
+              >
+                <IoMdClose size={25} color="white" onClick={handleClose} />
+              </div>
+            )}
           </div>
-        </form>
+        </div>
+
         {/* Search Suggestions Drop-Down */}
         <div className="absolute z-10 flex flex-col mt-2 p-2 overflow-hidden rounded bg-white">
           {searchData.map((data, index) => {
