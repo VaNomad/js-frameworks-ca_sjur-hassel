6a6ae04035d5c14a27d635a4a768071eafe3e996
VaNomad <sjur.hassel@gmail.com>
2023-05-26 12:12:45 +0100

    Adds styling updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,13 +8,19 @@ export default function Card({ products, title }) {
       </h2>
       <div className="">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-9 justify-center items-center text-black mb-8">
-          {products.map(({ id, title, imageUrl }) => (
+          {products.map(({ id, title, imageUrl, price }) => (
             <Link to={`/details/${id}`} key={id}>
               <div className="card-zoom flex flex-col justify-center items-center text-center p-4 border border-gray-400 w-full h-full m-3">
                 <div className="card-zoom-image">
                   <img src={imageUrl} alt={title} />
                 </div>
-                <h2 className="card-zoom-text font-bold">{title}</h2>
+                <h2 className="card-zoom-text bg-black text-white font-bold py-[30px] px-[50px]">
+                  {title}
+                </h2>
+                <h2 className="text-white font-bold card-zoom-text bg-black top-0 w-screen py-2">${price}</h2>
+                <h2 className="card-zoom-button border-1 border-gray-400 font-exa text-xl bottom-3">
+                  View Item
+                </h2>
               </div>
             </Link>
           ))}
