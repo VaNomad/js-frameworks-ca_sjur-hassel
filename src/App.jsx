3e90e7314f0a8b164374abdf1a0c05f0be327ece
VaNomad <sjur.hassel@gmail.com>
2023-05-29 22:35:39 +0100

    Adds back strictmode in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
 // Imports —————————————————————————————————
-// Css
 import "./App.css";
-// Components
-// import Layout from "./components/Layout";
-// import NavBar from "./components/header/NavBar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-import CheckOut from "./pages/CheckOut";
 import Details from "./pages/Details";
 import Four04 from "./pages/Four04";
-// Router
-// import { RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-
-// React-Router —————————————————————————————————
-// const router = createBrowserRouter([
-//   {
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Home />,
-//       },
-//       {
-//         path: "/contact",
-//         element: <Contact />,
-//       },
-//       {
-//         path: "/checkout",
-//         element: <CheckOut />,
-//       },
-//       {
-//         path: "/details/:id",
-//         element: <Details />,
-//       },
-//       {
-//         path: "*",
-//         element: <Four04 />,
-//       },
-//     ],
-//   },
-// ]);
-
-// App Component —————————————————————————————————
+import CheckOut from "./pages/CheckOut";
 
 function App() {
   return (
