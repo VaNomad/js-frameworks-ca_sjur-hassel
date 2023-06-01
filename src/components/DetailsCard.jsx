66a68a750964b30600513d953fb4581144c447ed
VaNomad <sjur.hassel@gmail.com>
2023-06-01 23:35:34 +0100

    cart page styling

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -26,15 +26,15 @@ export default function DetailsCard({data}) {
   // const quantity = 0;
 
   return (
-    <div className="h-screen flex flex-col">
+    <div className="flex h-screen flex-col">
       <div className="">
         <div className="relative flex items-center justify-end py-2">
           <MdArrowBack
             onClick={() => navigate(-1)}
             size={45}
-            className="fixed bg-white left-[-20px] top-[150px] sm:left-0 sm:top-[160px] cursor-pointer shadow-lg rounded-full mx-8 p-[8px]"
+            className="fixed left-[-20px] top-[150px] mx-8 cursor-pointer rounded-full bg-white p-[8px] shadow-lg sm:left-0 sm:top-[160px]"
           />
-          <h2 className="fixed md:absolute top-[155px] sm:top-[165px] md:top-[60px] md:z-[-1] text-center mb-2 font-semibold uppercase bg-black text-white py-2 px-6">
+          <h2 className="fixed top-[155px] mb-2 bg-black px-6 py-2 text-center font-semibold uppercase text-white sm:top-[165px] md:absolute md:top-[60px] md:z-[-1]">
             Item Details
           </h2>
         </div>
@@ -43,38 +43,60 @@ export default function DetailsCard({data}) {
         <div className="flex justify-center bg-cover">
           <img src={imageUrl} alt={title} />
         </div>
-        <div className="flex flex-col px-2 m- justify-center">
-          <h1 className="text-3xl p-3 border-b border-gray-500">{title}</h1>
+        <div className="m- flex flex-col justify-center px-2">
+          <h1 className="border-b border-gray-500 p-3 text-3xl">{title}</h1>
           <div className="p-3">{description}</div>
           <div>{reviews && reviews.rating}</div>
           <div className="p-3">Tags #{tags}</div>
-          <div className="text-xl font-bold hidden">{price}</div>
-          <div className="test-xl font-bold p-3">
+          <div className="hidden text-xl font-bold">{price}</div>
+          <div className="test-xl p-3 font-bold">
             {formatCurrency(discountedPrice)}
           </div>
           <div>
             {productQuantity === 0 ? (
-              <div to="" className="p-3">
-                <button onClick={() => cart.addOneToCart(data.id, data.title, data.imageUrl, data.price, data.discountedPrice)} className="w-full p-3 rounded-md text-white uppercase bg-gradient-to-b from-violet-800 to-fuchsia-600 px-3 py-2 hover:bg-none hover:border-2 hover:border-green-700 hover:text-black hover:scale-105 transition-all duration-100 ease-in-out">
+              <div className="p-3">
+                <button
+                  onClick={() =>
+                    cart.addOneToCart(
+                      data.id,
+                      data.title,
+                      data.imageUrl,
+                      data.price,
+                      data.discountedPrice
+                    )
+                  }
+                  className="hover:border-1 w-full rounded-md bg-gradient-to-b from-violet-800 to-fuchsia-600 p-3 px-3 py-2 text-xl font-light uppercase text-white transition-all duration-200 ease-in-out hover:border-fuchsia-700 hover:bg-none hover:text-fuchsia-700 hover:shadow-lg"
+                >
                   Add To Cart
                 </button>
               </div>
             ) : (
-              <div className="flex justify-between p-3 items-center">
-                <div className="flex justify-between items-center">
-                  <button onClick={() => cart.eraseOneFromCart(data.id)} className="w-8 h-8 flex justify-center items-center rounded bg-blue-500 text-white">
+              <div className="flex items-center justify-between p-3">
+                <div className="flex items-center justify-between">
+                  <button
+                    onClick={() => cart.eraseOneFromCart(data.id)}
+                    className="flex h-8 w-8 items-center justify-center rounded bg-blue-500 text-white"
+                  >
                     -
                   </button>
-                  <div className="text-gray-500 mx-4">
-                    <span className="text-2xl text-black">{productQuantity}</span> in
-                    cart
+                  <div className="mx-4 text-gray-500">
+                    <span className="text-2xl text-black">
+                      {productQuantity}
+                    </span>{" "}
+                    in cart
                   </div>
-                  <button onClick={() => cart.addOneToCart(data.id, data.price)} className="w-8 h-8 flex justify-center items-center rounded bg-blue-500 text-white">
+                  <button
+                    onClick={() => cart.addOneToCart(data.id, data.price)}
+                    className="flex h-8 w-8 items-center justify-center rounded bg-blue-500 text-white"
+                  >
                     +
                   </button>
                 </div>
                 <div>
-                  <button onClick={() => cart.deleteFromCart(data.id)} className="w-8 h-8 flex justify-center items-center rounded bg-red-700 text-white">
+                  <button
+                    onClick={() => cart.deleteFromCart(data.id)}
+                    className="flex h-8 w-8 items-center justify-center rounded bg-red-700 text-white"
+                  >
                     <BsTrash size={20} />
                   </button>
                 </div>
--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,5 +1,6 @@
 import { useShoppingCartContext } from "../context/ShoppingCartContext";
 import { BsTrash } from "react-icons/bs";
+import { BiPlus, BiMinus } from "react-icons/bi";
 import { formatCurrency } from "../utils/FormatCurrency";
 // import { useProductListContext } from "../context/ProductListContext";
 // import { useContext } from "react";
@@ -10,10 +11,10 @@ export default function CheckOut() {
   const cart = useShoppingCartContext();
   // const { products } = useProductListContext();
 
-   const productsCount = items.reduce(
-     (sum, product) => sum + product.quantity,
-     0
-   );
+  const productsCount = items.reduce(
+    (sum, product) => sum + product.quantity,
+    0
+  );
   return (
     <>
       <div className="flex h-screen flex-col items-center justify-center text-4xl font-bold text-black">
@@ -23,70 +24,82 @@ export default function CheckOut() {
           </>
         ) : (
           <div className="h-screen">
-            <h2 className="fixed right-0 top-[155px] z-[-1] mb-2 bg-black px-6 py-2 text-center font-semibold uppercase text-white sm:top-[165px] md:absolute md:top-[60px]">
-              Checkout
-            </h2>
+            {/* Checkout Label  */}
+            <div className="fixed right-0 top-[155px] z-[-1] mb-2 flex flex-col bg-black px-3 py-2 text-center text-base font-semibold uppercase text-white sm:flex-row sm:px-6 sm:py-3 sm:text-lg">
+              <span>check </span>
+              <span>out</span>
+            </div>
             {items.map((item) => (
               <div
                 key={item.id}
-                className="relative flex flex-col items-center justify-between border-b border-gray-300 p-3
-              "
+                className="relative mb-4 flex flex-col items-center justify-between border-b border-gray-300 p-3 md:w-[500px] md:flex-row lg:w-[900px]"
               >
+                {/* Image */}
                 <div>
                   <img
                     src={item.imageUrl}
                     alt={item.title}
-                    className="h-20 w-20 rounded-lg object-cover"
+                    className="m-3 h-[200px] w-[200px] rounded-lg object-cover"
                   />
                 </div>
-                <p className="text-lg font-normal text-gray-500">
-                  Price:{" "}
-                  {item.discountedPrice
-                    ? formatCurrency(item.discountedPrice)
-                    : formatCurrency(item.price)}
-                </p>
-                <div className="rounded-full bg-green-500 px-4 py-1 text-sm text-white">
-                  <span className="text-black">Discount: </span>
-                  {discountPercentage(item.discountedPrice, item.price)} %
-                </div>
-                <p className="text-lg font-normal text-black">
-                  x <span className="font-bold">{item.quantity}</span>
-                </p>
-                <div className="flex items-center justify-between p-3">
-                  <div className="flex items-center justify-between">
-                    <button
-                      onClick={() => cart.eraseOneFromCart(item.id)}
-                      className="flex h-8 w-8 items-center justify-center rounded bg-blue-500 text-white"
-                    >
-                      -
-                    </button>
-                    <div className="mx-4 text-gray-500">
-                      <span className="text-2xl text-black">
-                        {item.quantity}
-                      </span>{" "}
-                    </div>
-                    <button
-                      onClick={() => cart.addOneToCart(item.id, item.price)}
-                      className="flex h-8 w-8 items-center justify-center rounded bg-blue-500 text-white"
-                    >
-                      +
-                    </button>
+                <div className="flex flex-col lg:flex-row lg:flex-1 items-center justify-center lg:justify-around gap-4">
+                  {/* Title & Price */}
+                  <div className="flex flex-col justify-center">
+                    <p className="text-center text-lg">{item.title}</p>
+                    <p className="text-center text-lg font-normal text-gray-500">
+                      {" "}
+                      {item.discountedPrice
+                        ? formatCurrency(item.discountedPrice)
+                        : formatCurrency(item.price)}
+                    </p>
                   </div>
-                  <div>
-                    <button
-                      onClick={() => cart.deleteFromCart(item.id)}
-                      className="flex h-8 w-8 items-center justify-center rounded bg-red-700 text-white"
-                    >
-                      <BsTrash size={20} />
-                    </button>
+                  {/* Discount Overlay */}
+                  <div className="absolute right-0 top-3 flex items-center rounded-full bg-green-500 px-2 py-1 text-sm text-white md:right-[250px] lg:right-[650px]">
+                    <BiMinus />
+                    {discountPercentage(item.discountedPrice, item.price)} %
+                  </div>
+                  <div className="mt-2 flex flex-col items-center gap-2 md:gap-6">
+                    <div className="flex items-center justify-between">
+                      {/* Minus Button */}
+                      <button
+                        onClick={() => cart.eraseOneFromCart(item.id)}
+                        className="rounded bg-blue-500 text-white"
+                      >
+                        <BiMinus />
+                      </button>
+                      {/* Quantity */}
+                      <div className="mx-4 flex text-gray-500">
+                        <span className="text-2xl text-black">
+                          {item.quantity}
+                        </span>
+                      </div>
+                      {/* Plus Button */}
+                      <button
+                        onClick={() => cart.addOneToCart(item.id, item.price)}
+                        className="rounded bg-blue-500 text-white"
+                      >
+                        <BiPlus />
+                      </button>
+                    </div>
+                    {/* Trash Button */}
+                    <div>
+                      <button
+                        onClick={() => cart.deleteFromCart(item.id)}
+                        className="mt-2 flex h-9 w-9 items-center justify-center rounded bg-red-700 text-white md:mt-0 lg:mt-0"
+                      >
+                        <BsTrash size={25} />
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             ))}
-            <div className="flex flex-col">
-              <h1 className="text-xl">Total: {getTotalCost()}</h1>
-              <button className="bg-fuchsia-600 p-3 text-white">
-                Place Order
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-xl">
+                Total: {formatCurrency(getTotalCost())}
+              </h1>
+              <button className="hover:border-[1px] mt-4 w-full rounded-md bg-gradient-to-b from-violet-800 to-fuchsia-600 p-3 px-3 py-2 text-xl font-light uppercase tracking-widest text-white transition-all duration-200 ease-in-out hover:border-fuchsia-700 hover:bg-none hover:text-fuchsia-700 hover:shadow-lg">
+                Buy
               </button>
             </div>
           </div>
