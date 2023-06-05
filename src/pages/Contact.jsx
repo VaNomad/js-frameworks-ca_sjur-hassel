6e429c83df6694a6f132f41f3594bd578301e9d4
VaNomad <sjur.hassel@gmail.com>
2023-06-05 14:37:22 +0100

    Removes unnecessary code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,8 @@
 import ContactForm from "../components/ContactForm";
-// import TestForm from "../components/TestForm";
 
 export default function Contact() {
   return (
     <div>
-      {/* <TestForm /> */}
       <ContactForm />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,10 @@
 import ProductList from "../api/ProductList";
-// import Search from "../components/Search"
-// import SearchBar from "../components/SearchBar";
 import { ProductListProvider } from "../context/ProductListContext";
 
 export default function Home() {
   return (
     <ProductListProvider>
       <div>
-        {/* <Search /> */}
         <ProductList />
       </div>
     </ProductListProvider>
