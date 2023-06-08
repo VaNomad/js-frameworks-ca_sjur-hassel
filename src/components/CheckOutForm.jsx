83d4e267bae72d4463b41349b0c3e6b535408a6c
VaNomad <sjur.hassel@gmail.com>
2023-06-08 20:06:20 +0100

    Adds first part of checkoutform

--- src/components/CheckOutForm.jsx
+++ src/components/CheckOutForm.jsx
@@ -1,9 +1,13 @@
+import { useShoppingCartContext } from "../context/ShoppingCartContext";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { ToastContainer, Zoom, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ArrowBack from "./ArrowBack";
+// import creditcard from "../assets/creditcard.png";
 
-export default function ContactForm() {
+export default function CheckOutForm() {
+  const cart = useShoppingCartContext();
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
@@ -29,10 +33,10 @@ export default function ContactForm() {
     } else {
       newErrors.address = "";
     }
-    if (postcode.trim().length < 3) {
-      newErrors.address = "address needs to have at least 3 characters";
+    if (!validPostalCode(postcode)) {
+      newErrors.postcode = "address needs to have at least 3 characters";
     } else {
-      newErrors.address = "";
+      newErrors.postcode = "";
     }
     if (!validPhone(phone)) {
       newErrors.phone = "must be a valid Norwegian phone number";
@@ -48,7 +52,11 @@ export default function ContactForm() {
   }
 
   function validPhone(phone) {
-    return "^[A-Z0-9\\-]{5,8}$".test(phone);
+    return /^\d{8}$/.test(phone);
+  }
+
+  function validPostalCode(postcode) {
+    return /^\d{4}$/.test(postcode);
   }
 
   function onSubmit(e) {
@@ -59,10 +67,11 @@ export default function ContactForm() {
         fullName,
         email,
         address,
+        postcode,
         phone,
       };
       console.log(formData);
-      toast.success("Your phone Has Been Sent!", {
+      toast.success("Your Checkout was Successful!", {
         position: toast.POSITION.BOTTOM_CENTER,
         className: "toastStyle",
       });
@@ -89,133 +98,156 @@ export default function ContactForm() {
       setEmail(value);
       setErrors((prevErrors) => ({
         ...prevErrors,
-        email: !validEmail(value) ? "Invalid email address" : "",
+        email: !validEmail(value) ? "Must be a valid E-mail" : "",
       }));
     } else if (id === "address") {
       setAddress(value);
       setErrors((prevErrors) => ({
         ...prevErrors,
-        address: value.trim().length < 3 ? "Type at least 3 characters" : "",
+        address: value.trim().length < 10 ? "Must be a Norwegian Address" : "",
+      }));
+    } else if (id === "postcode") {
+      setPostCode(value);
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        postcode: !validPostalCode(value)
+          ? "Must be a valid Norwegian Postal Code"
+          : "",
       }));
     } else if (id === "phone") {
       setPhone(value);
       setErrors((prevErrors) => ({
         ...prevErrors,
-        phone: value.trim().length < 3 ? "Type at least 3 characters" : "",
+        phone: !validPhone(value)
+          ? "Must be a valid Norwegian phone number"
+          : "",
       }));
-    }
+    } 
   }
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <ArrowBack />
-      <form
-        onSubmit={onSubmit}
-        className="mx-8 mt-[80px] flex w-[600px] flex-col justify-center rounded-xl p-8 shadow-xl"
-      >
-        <div className="flex h-[100px] flex-col">
-          <label htmlFor="fullName" className="text-lg font-bold text-gray-600">
-            Full Name
-          </label>
-          <input
-            type="text"
-            id="fullName"
-            value={fullName}
-            placeholder="Your full Name"
-            onChange={onInputChange}
-            minLength={3}
-            required
-            className="rounded-md border border-gray-300 p-2"
-          />
-          {errors.fullName && (
-            <p className="animate-pulse text-fuchsia-600">{errors.fullName}</p>
-          )}
+    <div className="mt-[90px] text-center">
+      <div className="container mx-auto border">
+        <ArrowBack />
+        <div className="z-10 m-2 mx-auto max-w-[150px] rounded-full bg-black px-2 py-1 text-center text-xs font-semibold uppercase text-white xs:text-base">
+          <p>Check Out</p>
         </div>
-
-        <div className="flex h-[100px] flex-col">
-          <label htmlFor="email" className="text-lg font-bold text-gray-600">
-            E-mail
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            placeholder="Your E-mail"
-            onChange={onInputChange}
-            required
-            className="rounded-md border border-gray-300 p-2"
-          />
-          {errors.email && (
-            <p className="animate-pulse text-fuchsia-600">{errors.email}</p>
-          )}
-        </div>
-
-        <div className="flex h-[100px] flex-col">
-          <label htmlFor="address" className="text-lg font-bold text-gray-600">
-            address
-          </label>
-          <input
-            type="text"
-            id="address"
-            value={address}
-            placeholder="phone address"
-            onChange={onInputChange}
-            minLength={3}
-            required
-            className="rounded-md border border-gray-300 p-2"
-          />
-          {errors.address && (
-            <p className="animate-pulse text-fuchsia-600">{errors.address}</p>
-          )}
-        </div>
-
-        <div className="flex h-[100px] flex-col">
-          <label htmlFor="postcode" className="text-lg font-bold text-gray-600">
-            address
-          </label>
-          <input
-            type="text"
-            id="address"
-            value={address}
-            placeholder="phone address"
-            onChange={onInputChange}
-            minLength={3}
-            required
-            className="rounded-md border border-gray-300 p-2"
-          />
-          {errors.address && (
-            <p className="animate-pulse text-fuchsia-600">{errors.address}</p>
-          )}
-        </div>
-
-        <div className="flex h-[180px] flex-col">
-          <label htmlFor="phone" className="text-lg font-bold text-gray-600">
-            phone
-          </label>
-          <input
-            type="tel"
-            id="phone"
-            value={phone}
-            placeholder="Phone Number"
-            onChange={onInputChange}
-            minLength={11}
-            required
-            className="resize-none rounded-md border border-gray-300 p-2"
-          ></input>
-          {errors.phone && (
-            <p className="animate-pulse text-fuchsia-600">{errors.phone}</p>
-          )}
-        </div>
-
-        <button
-          type="submit"
-          onClick={onSubmit}
-          className="rounded-md bg-fuchsia-600 px-20 py-2 text-xl font-light uppercase tracking-widest text-white transition-all duration-200 ease-in-out hover:bg-fuchsia-500 hover:font-bold hover:tracking-widest hover:shadow-xl"
+        <form
+          onSubmit={onSubmit}
+          className="mx-8 mt-[80px] flex w-[600px] flex-col justify-center rounded-xl p-8 shadow-xl"
         >
-          Submit
-        </button>
-      </form>
-      <ToastContainer theme="light" transition={Zoom} />
+          {/* Full Name */}
+          <div className="relative flex flex-col">
+            <input
+              type="text"
+              id="fullName"
+              value={fullName}
+              placeholder="Full Name"
+              onChange={onInputChange}
+              minLength={3}
+              required
+              className="rounded-md border border-gray-300 p-2"
+            />
+            {errors.fullName && (
+              <p className="absolute right-3 top-2 animate-pulse text-fuchsia-600">
+                {errors.fullName}
+              </p>
+            )}
+          </div>
+
+          {/* Email */}
+          <div className="relative flex flex-col">
+            <input
+              type="email"
+              id="email"
+              value={email}
+              placeholder="E-mail"
+              onChange={onInputChange}
+              required
+              className="rounded-md border border-gray-300 p-2"
+            />
+            {errors.email && (
+              <p className="absolute right-3 top-2 animate-pulse text-fuchsia-600">
+                {errors.email}
+              </p>
+            )}
+          </div>
+
+          {/* Address */}
+          <div className="relative flex flex-col">
+            <input
+              type="text"
+              id="address"
+              value={address}
+              placeholder="Address"
+              onChange={onInputChange}
+              minLength={10}
+              required
+              className="rounded-md border border-gray-300 p-2"
+            />
+            {errors.address && (
+              <p className="absolute right-3 top-2 animate-pulse text-fuchsia-600">
+                {errors.address}
+              </p>
+            )}
+          </div>
+
+          {/* Postal Code */}
+          <div className="relative flex flex-col">
+            <input
+              type="tel"
+              id="postcode"
+              value={postcode}
+              placeholder="Postal Code"
+              onChange={onInputChange}
+              minLength={4}
+              required
+              className="rounded-md border border-gray-300 p-2"
+            />
+            {errors.postcode && (
+              <p className="absolute right-3 top-2 animate-pulse text-fuchsia-600">
+                {errors.postcode}
+              </p>
+            )}
+          </div>
+
+          {/* Phone Number */}
+          <div className="relative flex flex-col">
+            <input
+              type="tel"
+              id="phone"
+              value={phone}
+              placeholder="Phone Number"
+              onChange={onInputChange}
+              minLength={8}
+              required
+              className="resize-none rounded-md border border-gray-300 p-2"
+            ></input>
+            {errors.phone && (
+              <p className="absolute right-3 top-2 animate-pulse text-fuchsia-600">
+                {errors.phone}
+              </p>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            onClick={onSubmit}
+            className="rounded-md bg-fuchsia-600 px-20 py-2 text-xl font-light uppercase tracking-widest text-white transition-all duration-200 ease-in-out hover:bg-fuchsia-500 hover:font-bold hover:tracking-widest hover:shadow-xl"
+          >
+            Submit
+          </button>
+        </form>
+        <Link to="/checkoutsuccess">
+          <button
+            onClick={() => cart.deleteAllFromCart()}
+            className="buttonCta"
+          >
+            Buy
+          </button>
+        </Link>
+        <ToastContainer theme="light" transition={Zoom} />
+      </div>
     </div>
   );
 }
--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,28 +1,10 @@
-import { useShoppingCartContext } from "../context/ShoppingCartContext";
-import { Link } from "react-router-dom";
-import creditcard from "../assets/creditcard.png"
+import CheckOutForm from "../components/CheckOutForm";
+
 
 export default function CheckOut() {
-  const cart = useShoppingCartContext();
-
   return (
-    <div className="mt-[90px] text-center">
-      <div className="z-10 m-2 mx-auto max-w-[150px] rounded-full bg-black px-2 py-1 text-center text-xs font-semibold uppercase text-white xs:text-base">
-        <p>Check Out</p>
-      </div>
-      <div className="container mx-auto my-4 border">
-        <div className="">
-          
-        </div>
-        <Link to="/checkoutsuccess">
-          <button
-            onClick={() => cart.deleteAllFromCart()}
-            className="buttonCta"
-          >
-            Buy
-          </button>
-        </Link>
-      </div>
+    <div>
+      <CheckOutForm />
     </div>
   );
 }
