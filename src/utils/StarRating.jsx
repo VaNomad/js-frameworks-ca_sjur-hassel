e8f770cbdda91756e863e73898f87399455655fa
VaNomad <sjur.hassel@gmail.com>
2023-06-08 14:51:52 +0100

    Cleanup

--- src/utils/StarRating.jsx
+++ src/utils/StarRating.jsx
@@ -1,24 +1,19 @@
-import React from "react";
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
 export default function StarRating({ rating }) {
   const hasHalfStar = rating % 1 !== 0 ? 1 : 0;
   const filledStarsCount = Math.ceil(rating) - hasHalfStar;
   const emptyStarsCount = 5 - Math.floor(rating) - hasHalfStar;
-
-  console.log(hasHalfStar)
-  console.log(filledStarsCount)
-  console.log(emptyStarsCount)
-  console.log(typeof rating)
-  console.log(rating)
-
   const stars = [];
+
   for (let i = 0; i < filledStarsCount; i++) {
     stars.push(<FaStar key={i} />);
   }
+
   if (hasHalfStar) {
     stars.push(<FaStarHalfAlt key="half" />);
   }
+
   for (let i = 0; i < emptyStarsCount; i++) {
     stars.push(<FaRegStar key={i} />);
   }
