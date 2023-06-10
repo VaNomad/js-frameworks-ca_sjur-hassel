e7a568e8cf1df18d906da87a314b6260592c2055
VaNomad <sjur.hassel@gmail.com>
2023-06-10 12:21:09 +0100

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <div className="w-full bg-black p-5 text-white text-3xl text-center uppercase">
-      Footer
-    </div>
+    <footer>
+      <div className="w-full bg-black p-5 text-white text-3xl text-center uppercase">
+        Footer
+      </div>
+    </footer>
   )
 }
