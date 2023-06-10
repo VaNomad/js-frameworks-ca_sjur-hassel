d5044cd2684e1879e5fb85991d4d8726289934fd
VaNomad <sjur.hassel@gmail.com>
2023-06-10 17:55:31 +0100

    style update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import sjurblue from "../assets/sjurioblue.png"
 export default function Footer() {
   return (
     <footer>
-      <div className="w-full bg-violet-100 pb-5 md:p-5">
-        <div className="flex flex-col md:flex-row items-center justify-center md:gap-8">
+      <div className="w-full bg-violet-100 p-5">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-3 md:gap-8">
           <img src={sjurblue} alt="" className="w-[50px]" />
           <p className="font-exa text-sm font-thin uppercase">
             © sjur hassel 2023
