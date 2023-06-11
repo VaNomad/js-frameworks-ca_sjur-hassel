043e511c99b4bdcfd523e5b08e95f01b41b320a8
VaNomad <sjur.hassel@gmail.com>
2023-06-11 15:16:38 +0100

    footer updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 // import sjurblue from "../assets/sjurioblue.png"
 import sjuriowhite from "../assets/sjuriowhite.png"
+import {AiOutlineGithub, AiOutlineLinkedin} from "react-icons/ai"
 
 export default function Footer() {
   return (
     <footer className="my-10 p-4 font-karla text-sm">
-      <div className="w-full bg-violet-400 p-5 text-white rounded-xl">
+      <div className="w-full rounded-xl bg-violet-500 p-5 text-white">
         <div className="flex flex-col items-center justify-around gap-3 md:flex-row md:gap-8">
           <p className="font-exa">© Sjur Hassel 2023</p>
+          <AiOutlineGithub size={20} href="https://github.com/VaNomad" />
           <img src={sjuriowhite} alt="" className="w-[50px]" />
+          <AiOutlineLinkedin
+            size={20}
+            href="https://www.linkedin.com/in/sjurhassel/"
+          />
           <p>Built with React.js & TailwindCss</p>
         </div>
       </div>
